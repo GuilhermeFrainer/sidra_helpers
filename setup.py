24ddf372235d17d2200ea150d27ad72ece8342c2
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-01-09 09:56:14 -0300

    Attempt at eliminating 'src' from import statement

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sidra_helpers",
+    version="1.0.1",
+    author="Guilherme da Silva Frainer",
+    author_email="guilherme.sfrainer@gmail.com",
+    description="Helper functions made for personal use to work with the SIDRA API",
+    url="https://github.com/GuilhermeFrainer/sidra_helpers",
+    packages=find_packages(),
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from src import sidra_helpers
